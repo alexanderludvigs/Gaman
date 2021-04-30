064c2f844741b9c27879b346163e4800a2c29902
Alexander <alexander.ludvigsson@arionbanki.is>
2021-04-30 20:54:46 +0000

    Initial Commit

---
+++ Gaman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gaman
+//
+//  Created by Alexander Ludvigsson on 30.4.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
